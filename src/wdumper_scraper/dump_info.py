7fc87b848129c82af462ee06d50e7099d3dabe03
Itamar Givon <itamar.givon@wikimedia.de>
2026-03-20 09:58:56 +0100

    Add language mapping

--- src/wdumper_scraper/dump_info.py
+++ src/wdumper_scraper/dump_info.py
@@ -14,17 +14,20 @@ class DumpInfo:
             "id": dump["id"],
             "name": dump["title"],
             "url": url,
+            "languages": self.__map_languages(),
             **self.__map_filters()
         }
 
     @property
-    def data(self) -> dict[str, str | int]:
+    def data(self) -> dict[str, str | int ]:
         return self.__data
 
-    def __map_filters(self):
+    def __map_filters(self) -> dict[str, str]:
         filter_keys: list[FilterKey] = ["labels", "descriptions", "aliases", "sitelinks"]
 
         return {
             key: "yes" if self.__spec[key] else "no" for key in filter_keys
         }
 
+    def __map_languages(self) -> str:
+        return " | ".join(self.__spec.get("languages") or [])
